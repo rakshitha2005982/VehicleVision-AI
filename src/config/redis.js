eb8fde206d1a2c6a55b09e724a81c643d871a15b
Rakshitha Prabhu <rakshhh2005@gmail.com>
2026-07-22 08:04:07 +0530

    Configure Upstash Redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,17 +1,20 @@
 const IORedis = require("ioredis");
 
 const connection = new IORedis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379,
-    maxRetriesPerRequest: null
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  tls: {}, // Required for Upstash
+  maxRetriesPerRequest: null,
 });
 
 connection.on("connect", () => {
-    console.log("✅ Redis Connected Successfully");
+  console.log("✅ Redis Connected Successfully");
 });
 
 connection.on("error", (err) => {
-    console.error("❌ Redis Error:", err);
+  console.error("❌ Redis Error:", err);
 });
 
 module.exports = connection;
